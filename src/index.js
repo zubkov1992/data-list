228bd3a92f88cdf3a1c2f4e69e521bbf7dbf2b12
Andrei Zubkov <zubkov1992@gmail.com>
2018-04-06 00:08:00 +0600

    Приведение к окончательно правильному виду.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,4 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './pages/App';
-import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+export {default as AppBarPages} from './components/AppBarPages';
+export {default as BottomNavigationPages} from './components/BottomNavigationPages';
+export {default as CardDataMaster} from './components/CardDataMaster';
+export {default as DrawerPages} from './components/DrawerPages';
