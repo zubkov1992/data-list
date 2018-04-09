57cad4a66865117a636aa58245c9ab77a48496b4
Andrei Zubkov <zubkov1992@gmail.com>
2018-04-09 16:20:57 +0600

    Новый компонент.

--- src/index.js
+++ src/index.js
@@ -2,3 +2,4 @@ export {default as AppBarPages} from './AppBarPages';
 export {default as BottomNavigationPages} from './BottomNavigationPages';
 export {default as CardDataMaster} from './CardDataMaster';
 export {default as DrawerPages} from './DrawerPages';
+export {default as ErrorPages} from './ErrorPages';
